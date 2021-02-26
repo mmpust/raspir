17c82e17c87b2e8d65394e245fb226b2fea080de
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2021-02-26 17:19:00 +0100

    propose splitting SAM and BAM files to allow BAM entrypoint

--- run_SLURM_file_prep.sh
+++ run_SLURM_file_prep.sh
@@ -2,64 +2,84 @@
 
 ## Marie-Madlen Pust
 ## Last updated: 27 January 2021
-## pust.marie-madlen@mh-hannover.de
+## pust.marie-madlen@mh-hannover.de   ### Remove this to avoid this being scraped for SPAM -- contact via github is better !
 
 # set partition
 #SBATCH -p normal
 
 # set run on x MB node only
-#SBATCH --mem 40000
+#SBATCH --mem 30000
 
 # set run x cpus
-#SBATCH --cpus-per-task 12
+#SBATCH --cpus-per-task 8
 
 # set name of job
 #SBATCH --job-name=raspir_prepare
 
 # Add miniconda3 to PATH
-. /miniconda3/etc/profile.d/conda.sh
+. /mnt/ngsnfs/tools/miniconda3/etc/profile.d/conda.sh
 
 # Activate env on cluster node
 conda activate raspir_env >> /dev/null
 
-for items in *.sam
+cpus=8
+human_chr_pattern="1_1_1"
+
+# Only run SAM section if SAM files exist in the current directory
+count=`ls -1 *.sam 2>/dev/null | wc -l`
+if [ $count != 0 ]
+    then
+    # run SAM conversion
+
+	# For pipelines which start from SAM. Many pipelines will start from BAM.
+	for items in *.sam
+		do
+			echo $items
+			fname=$(echo ${items} | sed 's/.sam//')
+			echo $fname
+
+
+				# Remove reads with low mapping quality
+				samtools view -hM -q 20 $items > ${items%.sam}.mq20.sam
+
+				# Convert file from SAM to BAM format
+				samtools view -h -b -S ${items%.sam}.mq20.sam  > ${fname}.bam
+
+				# Discard unmapped sequences @Marie are these not removed by the mq20 filter, I think so ?
+				samtools view -b -F 4 $items > ${fname}_1.bam
+
+				# Sort bam file
+				samtools sort @ $cpus -${fname}_1.bam -o ${fname}.sorted.bam
+		done
+fi
+
+# Start from sorted mq20 BAM with no unmapped sequences  
+for items in *.bam
 	do
 		echo $items
-		fname=$(echo ${items} | sed 's/.sam//')
+		fname=$(echo ${items} | sed 's/.bam//')
 		echo $fname
 
-    		# Remove reads with low mapping quality
-    		samtools view -hM -q 20 $items > ${items%.sam}.mq20.sam
+   		# Obtain coverage information
+   		#samtools depth ${fname}.sorted.bam > ${fname}.sorted.raspir1.csv
+   		samtools depth ${fname}.bam | grep -v $human_chr_pattern  > ${fname}.raspir1.csv
 
-    		# Convert file from SAM to BAM format
-    		samtools view -h -b -S ${items%.sam}.mq20.sam  > ${fname}.bam
+   		# Add genome size
+		   ### COLIN TODO - items here is for SAM format , sed fails on bam
+   		samtools view -H ${items} | sed 's/LN://g' > ${fname}.genomeSize_1.csv
+   		sed -i 's/SN://g' ${fname}.genomeSize_1.csv
+   		cut -f2- ${fname}.genomeSize_1.csv > ${fname}.genomeSize.csv
 
-    		# Discard unmapped sequences
-    		samtools view -b -F 4 $items > ${fname}_1.bam
-
-    		# Sort bam file
-    		samtools sort ${fname}_1.bam -o ${fname}.sorted.bam
-
-    		# Generate index
-    		samtools index ${fname}.sorted.bam
-
-    		# Obtain coverage information
-    		samtools depth ${fname}.sorted.bam > ${fname}.sorted.raspir1.csv
-
-    		# Add genome size
-    		sed 's/LN://g' ${items} > ${fname}.genomeSize_1.csv
-    		sed -i 's/SN://g' ${fname}.genomeSize_1.csv
-    		cut -f2- ${fname}.genomeSize_1.csv > ${fname}.genomeSize.csv
-
-    		# Add column with genome size
-    		awk -v FS="\t" -v OFS="\t" 'FNR==NR{a[$1]=$2;next;} {if(a[$1]) {print a[$1], $0} else {print "NA",$0}}' \
-		${fname}.genomeSize.csv ${fname}.sorted.raspir1.csv > ${fname}.sorted.raspir.csv
+   		# Add column with genome size
+   		#awk -v FS="\t" -v OFS="\t" 'FNR==NR{a[$1]=$2;next;} {if(a[$1]) {print a[$1], $0} else {print "NA",$0}}' ${fname}.genomeSize.csv ${fname}.raspir1.csv > ${fname}.raspir.csv
+		awk -v FS="\t" -v OFS="\t" 'FNR==NR{a[$1]=$2;next;} {if(a[$1]) {print a[$1], $0} else {print "NA",$0}}' \
+		${fname}.genomeSize.csv ${fname}.raspir1.csv > ${fname}.raspir.csv
 
 		# Convert into a comma-separated file
-		sed -i 's/\t/,/g' ${fname}.sorted.raspir.csv
+		sed -i 's/\t/,/g' ${fname}.raspir.csv
 		# Add header
-		sed -i '1iGenomeLength,Organism,Position,Depth\' ${fname}.sorted.raspir.csv
+		sed -i '1iGenomeLength,Organism,Position,Depth\' ${fname}.raspir.csv
 
 		# Remove intermediate files
-		rm ${fname}.sorted.raspir1.csv ${fname}.genomeSize_1.csv ${fname}.genomeSize.csv
+		#rm ${fname}.raspir1.csv ${fname}.genomeSize_1.csv ${fname}.genomeSize.csv
 	done
