2605193a725fe531f303025daf5ebb4dd311bb16
Marie-Madlen Pust <74159013+mmpust@users.noreply.github.com>
2021-11-16 08:41:46 +0100

    Update runbatch_raspir_wochenende_SLURM.sh

--- raspir_with_wochenende/runbatch_raspir_wochenende_SLURM.sh
+++ raspir_with_wochenende/runbatch_raspir_wochenende_SLURM.sh
@@ -3,7 +3,7 @@
 # Marie Pust
 # last updated: 04 August 2021
 
-for i in `ls *.ndp.trm.s.mm.dup.mq30.raspir.csv`
+for i in `ls *.raspir.csv`
 	do
 		echo $i
 		sbatch -c 16 run_raspir_wochenende_SLURM.sh $i
