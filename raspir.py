31ce099e95cae28a6511306b69f446284ff474d7
Colin Davenport <colindaven@hotmail.com>
2021-12-09 11:46:08 +0100

    use startswith not contains

--- raspir.py
+++ raspir.py
@@ -316,8 +316,8 @@ def process_csv(file_name, out_prefix, args):
         df = df[~filter_approach]
         logging.info('1a) Human reads have been removed')
         # filtering  reads attributed to organisms starting with chr eg some human, mouse chrs
-        pattern_del = '^chr'
-        filter_approach = df['Organism'].str.contains(pattern_del, na=False)
+        pattern_del = 'chr'
+        filter_approach = df['Organism'].str.startswith(pattern_del, na=False)
         df = df[~filter_approach]
         logging.info('1b) Human chr reads have been removed')
 
